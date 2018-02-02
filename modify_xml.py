9fccd59862fca14468c66301030d07b0177d4e8f
madi <dileomargherita@gmail.com>
2018-02-02 15:46:18 +0100

    loops over files in folder, excluding xml containing georef

--- modify_xml.py
+++ modify_xml.py
@@ -8,63 +8,93 @@ __email__ = "dileomargherita@gmail.com"
 import xml.etree.ElementTree as ET
 import os
 
-FILENAME = 'pt620000-4405000_dmc_s1.xml'
+#FILENAME = ".."
 
 # Source folder
 FILEPATH = '/run/media/madi/TOSHIBA EXT/DMC/jpg_coarser'
-
 # Destination folder
 DESTPATH = '/run/media/madi/TOSHIBA EXT/DMC/jpg_coarser/modified'
-
 # Original resolution
 RES_ORIGINAL = 0.15
 # Current resolution
 RES_CURRENT  = 0.60
 
-factor = RES_CURRENT / RES_ORIGINAL
+#-----------------------------------------------------------------------
 
+factor = RES_CURRENT / RES_ORIGINAL
 os.chdir(FILEPATH)
 
-tree = ET.parse(FILENAME)
-root = tree.getroot()
+#-----------------------------------------------------------------------
 
-# Update size of current image
-orig_img_width = int(root.find("./size/width").text)
-curr_img_width = int(round(orig_img_width / factor))
-root.find("./size/width").text = str(curr_img_width)
-root.find("./size/width").set('updated', 'yes')
-
-orig_img_height = int(root.find("./size/height").text)
-curr_img_height = int(round(orig_img_height / factor))
-root.find("./size/height").text = str(curr_img_height)
-root.find("./size/height").set('updated', 'yes')
-
-
-# Update size of each object
-for obj in root.findall("./object/bndbox/xmin"):
-    orig_xmin = int(obj.text)
-    curr_xmin = int(round(orig_xmin / factor))
-    obj.text = str(curr_xmin)
-    obj.set('updated', 'yes')
+def CreateFileList(FILEPATH):
+    for filename in os.listdir(str(FILEPATH)):
+        if filename.endswith(".xml") and "aux" not in filename:
+            fileList.append(filename)
+    fileList.sort()
+    print fileList
+    return fileList
     
-for obj in root.findall("./object/bndbox/ymin"):
-    orig_ymin = int(obj.text)
-    curr_ymin = int(round(orig_ymin / factor))
-    obj.text = str(curr_ymin)
-    obj.set('updated', 'yes')
+#-----------------------------------------------------------------------
 
-for obj in root.findall("./object/bndbox/xmax"):
-    orig_xmax = int(obj.text)
-    curr_xmax = int(round(orig_xmax / factor))
-    obj.text = str(curr_xmax)
-    obj.set('updated', 'yes')
+def updateXml(FILENAME, DESTPATH):
+    tree = ET.parse(FILENAME)
+    root = tree.getroot()
 
-for obj in root.findall("./object/bndbox/ymax"):
-    orig_ymax = int(obj.text)
-    curr_ymax = int(round(orig_ymax / factor))
-    obj.text = str(curr_ymax)
-    obj.set('updated', 'yes')
+    # Update size of current image
+    orig_img_width = int(root.find("./size/width").text)
+    curr_img_width = int(round(orig_img_width / factor))
+    root.find("./size/width").text = str(curr_img_width)
+    root.find("./size/width").set('updated', 'yes')
 
-os.chdir(DESTPATH)
-tree.write(FILENAME)
+    orig_img_height = int(root.find("./size/height").text)
+    curr_img_height = int(round(orig_img_height / factor))
+    root.find("./size/height").text = str(curr_img_height)
+    root.find("./size/height").set('updated', 'yes')
 
+    # Update size of each object
+    for obj in root.findall("./object/bndbox/xmin"):
+        orig_xmin = int(obj.text)
+        curr_xmin = int(round(orig_xmin / factor))
+        obj.text = str(curr_xmin)
+        obj.set('updated', 'yes')
+    
+    for obj in root.findall("./object/bndbox/ymin"):
+        orig_ymin = int(obj.text)
+        curr_ymin = int(round(orig_ymin / factor))
+        obj.text = str(curr_ymin)
+        obj.set('updated', 'yes')
+
+    for obj in root.findall("./object/bndbox/xmax"):
+        orig_xmax = int(obj.text)
+        curr_xmax = int(round(orig_xmax / factor))
+        obj.text = str(curr_xmax)
+        obj.set('updated', 'yes')
+
+    for obj in root.findall("./object/bndbox/ymax"):
+        orig_ymax = int(obj.text)
+        curr_ymax = int(round(orig_ymax / factor))
+        obj.text = str(curr_ymax)
+        obj.set('updated', 'yes')
+
+    os.chdir(DESTPATH)
+    tree.write(FILENAME)
+
+#-----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    
+    # Loop over files in source folders
+    fileList = []
+    
+    fileList = CreateFileList(FILEPATH)
+    for filename in fileList:
+        updateXml(filename, FILEPATH)
+        
+    print "Updated xml files saved in ", DESTPATH
+    
+    
+    
+    
+    
+    
+    
